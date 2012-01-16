50b0f6fb23649b4dd9675697a9b5347d2abdddde
Brandon Williams <brandon@opetopic.com>
2012-01-16 00:04:25 -0500

    Fixed headers.

--- OPQuartzKit/Source/Quartz+Opetopic.m
+++ OPQuartzKit/Source/Quartz+Opetopic.m
@@ -3,7 +3,7 @@
 //  OPQuartzKit
 //
 //  Created by Brandon Williams on 1/14/12.
-//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//  Copyright (c) 2012 Opetopic. All rights reserved.
 //
 
 #import "Quartz+Opetopic.h"
