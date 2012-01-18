d49ed02070eedb65a2c59da693e917daeeecc4ca
Brandon Williams <brandon@kickstarter.com>
2012-01-18 11:59:40 -0500

    Added a header for including everything.

---
+++ OPQuartzKit/Source/OPQuartzKit.h
@@ -0,0 +1,12 @@
+//
+//  OPQuartzKit.h
+//  OPQuartzKit
+//
+//  Created by Brandon Williams on 1/18/12.
+//  Copyright (c) 2012 Opetopic. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+#import "OPGradient.h"
+#import "Quartz+Opetopic.h"
